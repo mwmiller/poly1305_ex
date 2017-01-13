a28ef67629df4c5938861b56ed925648d22e34ae
Matt Miller <mwm@fastmail.com>
2017-01-12 20:10:13 -0500

    Bump to 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Poly1305.Mixfile do
 
   def project do
     [app: :poly1305,
-     version: "0.4.5",
+     version: "1.0.0",
      elixir: "~> 1.4",
      name: "Poly1305",
      source_url: "https://github.com/mwmiller/poly1305_ex",
@@ -20,8 +20,8 @@ defmodule Poly1305.Mixfile do
 
   defp deps do
     [
-      {:chacha20, "~> 0.3"},
-      {:equivalex, "~> 0.1"},
+      {:chacha20, "~> 1.0"},
+      {:equivalex, "~> 1.0"},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
     ]
